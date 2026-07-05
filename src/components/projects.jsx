44060e0686d5fd2d9404b884a70739e1cf377bdb
Abdur Rahman Tusahr <abdurrahmantushar0@gmail.com>
2026-07-05 16:11:52 +0600

    Update projects.jsx
    Web site link update and some changes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,26 +10,35 @@ export const Projects = () => {
 
             <h1 className="lg:text-3xl text-x font-bold mb-12 flex items-center gap-2">
                 <CgDanger className="w-8 h-8" />
-                I’ve created 10+ practice projects, and I’ll be deploying them one by one very soon.
-            </h1>
+                Featured Projects  </h1>
 
             <div className="columns-1 sm:columns-2 lg:columns-3 gap-6 space-y-6">
 
                 <div className="break-inside-avoid h-[350px] ml-5 w-[250px] lg:h-[370px] lg:ml-5 lg:w-[350px] bg-white shadow-md rounded-xl p-4 hover:shadow-xl transition">
                     <img src={picture2} className="rounded-lg h-[150px] w-[200px] lg:h-[200px] lg:w-[300px]" alt="project" />
 
-                    <h2 className="mt-4 font-semibold">E-Commerce UI</h2>
+                    <h2 className="mt-4 font-semibold"> Full Stack E-Commerce Platform </h2>
 
                     <p className="text-sm mt-2 text-gray-600">
-                        Fully responsive shop UI with filtering, pagination, API data, and dynamic components.
-                    </p>
+            A modern MERN-based e-commerce application featuring responsive UI,
+            REST API integration, product browsing, authentication, shopping
+            cart functionality, and a clean user experience.                    </p>
 
                     <a
                         href="https://abdur-e-commerce-1.vercel.app/"
                         target="_blank"
+                        rel="noopener noreferrer"
                         className="mt-3 inline-block px-3 py-1 bg-black text-white rounded-md text-sm"
                     >
-                        Live Preview
+                        Live Demo
+                    </a>
+                    <a
+                      href="https://github.com/abdurrahmantushar"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="px-4 py-2 border border-black text-black rounded-md text-sm hover:bg-black hover:text-white transition"
+                    >
+                      GitHub
                     </a>
                 </div>
 
