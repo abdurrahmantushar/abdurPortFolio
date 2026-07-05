f6e8dd28bfd085f7d818c406594c08cfbcf92b81
Abdur Rahman Tusahr <abdurrahmantushar0@gmail.com>
2026-07-05 15:56:59 +0600

    Update some info

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,28 +6,44 @@ import { FaInstagram } from "react-icons/fa";
 export const Contact=()=>{
     return(
         <div className="h-[305px] bg-black text-white font-mono">
-            <h1 className="lg:ml-10 ml-5 text-xs mt-10">Contact Deatils:</h1>
+            <h1 className="lg:ml-10 ml-5 text-xs mt-10">Contact Details</h1>
             <div>
                 <p className="mt-10 flex gap-1 justify-start md:justify-center lg:justify-center px-4"> 
                 <HiOutlineMail className="mt-1"/>
-                E-mail: abdurrahmantushar0@gmail.com </p>
+                Email: abdurrahmantushar0@gmail.com </p>
                 <div className="flex justify-center gap-4 mt-4">
-      <a href="https://github.com/abdurrahmantushar" className="hover:text-gray-300 flex gap-1">
+      <a href="https://abdur-folio.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-gray-300 flex gap-1"
+            >
+        HiOutlineGlobeAlt className="mt-1" />
+        Portfolio
+        </a>
+      <a href="https://github.com/abdurrahmantushar" 
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-gray-300 flex gap-1">
       <FaGithubSquare className=" mt-1" />
       GitHub</a>
       <a href="https://www.linkedin.com/in/abdur-rahman-tushar-x/"
-       className="hover:text-gray-300 flex gap-1">
+         target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-300 flex gap-1">
       <CiLinkedin className=" mt-1"/>
       LinkedIn</a>
-      <a href="https://www.instagram.com/abdur_rahman_tushar_/" className="hover:text-gray-300 flex gap-1">
+      <a href="https://www.instagram.com/abdur_rahman_tushar_/" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-300 flex gap-1">
       <FaInstagram className="mt-1"/>
-      Instgram</a>
+      Instagram</a>
     </div>
-    <p className="  flex ml-10 lg:justify-center lg:items-center">Number: +880 187 073 3450</p>
+    <p className="  flex ml-10 lg:justify-center lg:items-center">Phone: +880 189 013 0921</p>
             </div>
            <div className="flex justify-end items-end p-5 pt-25 ">
   <p className="text-sm text-gray-500">
-    © 2025 Abdur Rahman Tushar — All Rights Reserved.
+    © 2026 Abdur Rahman Tushar. Built with React & Tailwind CSS.
   </p>
 </div>
 
